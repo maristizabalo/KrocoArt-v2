1e518a5235cb621a2bfc68c2e5b1e680ec062629
maristizabalo <maristizabalo95@gmail.com>
2024-11-26 23:57:21 -0500

    Ajustes iniciales, pagina Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,49 +1,50 @@
-import { Link } from "react-router-dom";
-// import { useGetProductsQuery } from "../redux/api/productApiSlice";
+import { Link, useParams } from "react-router-dom";
+import { useGetProductsQuery } from "../redux/api/productApiSlice";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
-import { useState } from "react";
-// import Product from "./Products/Product";
+import Product from "./Products/Product";
 
 const Home = () => {
-  // const { keyword } = useParams();
-  // const { data, isLoading, isError } = useGetProductsQuery({ keyword });
+  const { keyword } = useParams();
+  const { data, isLoading, isError } = useGetProductsQuery({ keyword });
+  console.log(data, isLoading, isError)
+  console.log(keyword)
 
   return (
     <>
-      {/* {!keyword ? <Header /> : null} */}
-      {/* {isLoading ? ( */}
-        {/* <Loader /> */}
-      {/* ) : isError ? ( */}
-        {/* <Message variant="danger">
+       {!keyword ? <h1>TITULO</h1> : null}
+      {isLoading ? (
+        <Loader />
+      ) : isError ? (
+        <Message variant="danger">
           {isError?.data.message || isError.error}
         </Message>
-      ) : ( */}
+      ) : (
         <>
-          <div className="">
-            <h1 className="">
+          <div className="flex justify-between items-center">
+            <h1 className="ml-[20rem] mt-[10rem] text-[3rem]">
               Special Products
             </h1>
 
             <Link
               to="/shop"
-              className=""
+              className="bg-pink-600 font-bold rounded-full py-2 px-10 mr-[18rem] mt-[10rem]"
             >
               Shop
             </Link>
           </div>
 
           <div>
-            <div className="">
-              {/* {data.products.map((product) => (
+            <div className="flex justify-center flex-wrap mt-[2rem]">
+              {data.map((product) => (
                 <div key={product._id}>
                   <Product product={product} />
                 </div>
-              ))} */}
+              ))}
             </div>
           </div>
         </>
-      {/* )} */}
+      )}
     </>
   );
 };
--- frontend/src/redux/api/productApiSlice.js
+++ frontend/src/redux/api/productApiSlice.js
@@ -1,11 +1,11 @@
-import { PRODUCT_URL, UPLOAD_URL } from "../constants";
+import { PRODUCTS_URL } from "../constants";
 import { apiSlice } from "./apiSlice";
 
 export const productApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: ({ keyword }) => ({
-        url: `${PRODUCT_URL}`,
+        url: `${PRODUCTS_URL}`,
         params: { keyword },
       }),
       keepUnusedDataFor: 5,
@@ -13,26 +13,26 @@ export const productApiSlice = apiSlice.injectEndpoints({
     }),
 
     getProductById: builder.query({
-      query: (productId) => `${PRODUCT_URL}/${productId}`,
+      query: (productId) => `${PRODUCTS_URL}/${productId}`,
       providesTags: (result, error, productId) => [
         { type: "Product", id: productId },
       ],
     }),
 
     allProducts: builder.query({
-      query: () => `${PRODUCT_URL}/allProducts`,
+      query: () => `${PRODUCTS_URL}/allProducts`,
     }),
 
     getProductDetails: builder.query({
       query: (productId) => ({
-        url: `${PRODUCT_URL}/${productId}`,
+        url: `${PRODUCTS_URL}/${productId}`,
       }),
       keepUnusedDataFor: 5,
     }),
 
     createProduct: builder.mutation({
       query: (productData) => ({
-        url: `${PRODUCT_URL}`,
+        url: `${PRODUCTS_URL}`,
         method: "POST",
         body: productData,
       }),
@@ -41,23 +41,23 @@ export const productApiSlice = apiSlice.injectEndpoints({
 
     updateProduct: builder.mutation({
       query: ({ productId, formData }) => ({
-        url: `${PRODUCT_URL}/${productId}`,
+        url: `${PRODUCTS_URL}/${productId}`,
         method: "PUT",
         body: formData,
       }),
     }),
 
-    uploadProductImage: builder.mutation({
-      query: (data) => ({
-        url: `${UPLOAD_URL}`,
-        method: "POST",
-        body: data,
-      }),
-    }),
+    // uploadProductImage: builder.mutation({
+    //   query: (data) => ({
+    //     url: `${UPLOAD_URL}`,
+    //     method: "POST",
+    //     body: data,
+    //   }),
+    // }),
 
     deleteProduct: builder.mutation({
       query: (productId) => ({
-        url: `${PRODUCT_URL}/${productId}`,
+        url: `${PRODUCTS_URL}/${productId}`,
         method: "DELETE",
       }),
       providesTags: ["Product"],
@@ -65,25 +65,25 @@ export const productApiSlice = apiSlice.injectEndpoints({
 
     createReview: builder.mutation({
       query: (data) => ({
-        url: `${PRODUCT_URL}/${data.productId}/reviews`,
+        url: `${PRODUCTS_URL}/${data.productId}/reviews`,
         method: "POST",
         body: data,
       }),
     }),
 
     getTopProducts: builder.query({
-      query: () => `${PRODUCT_URL}/top`,
+      query: () => `${PRODUCTS_URL}/top`,
       keepUnusedDataFor: 5,
     }),
 
     getNewProducts: builder.query({
-      query: () => `${PRODUCT_URL}/new`,
+      query: () => `${PRODUCTS_URL}/new`,
       keepUnusedDataFor: 5,
     }),
 
     getFilteredProducts: builder.query({
       query: ({ checked, radio }) => ({
-        url: `${PRODUCT_URL}/filtered-products`,
+        url: `${PRODUCTS_URL}/filtered-products`,
         method: "POST",
         body: { checked, radio },
       }),
@@ -102,6 +102,6 @@ export const {
   useCreateReviewMutation,
   useGetTopProductsQuery,
   useGetNewProductsQuery,
-  useUploadProductImageMutation,
+  // useUploadProductImageMutation,
   useGetFilteredProductsQuery,
 } = productApiSlice;
